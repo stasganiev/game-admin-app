5d079cabbe7e651784dbe2b2666105ac446302e6
Stas Ganiev <stas@ganiev.pro>
2022-11-18 15:55:32 +0100

    Live up the button to toggle menu visible

--- js/main.js
+++ js/main.js
@@ -1,2 +1,16 @@
-"use strict";
+'use strict';
 
+const btnToggleMenu = document.querySelector('.menu__toggle-btn');
+
+btnToggleMenu.addEventListener('click', (evt) => {
+  evt.preventDefault();
+
+  const menuElement = document.querySelector('.menu');
+  if (menuElement.classList.contains('menu-js-opened')) {
+    menuElement.classList.remove('menu-js-opened');
+    menuElement.classList.add('menu-js-closed');
+  } else {
+    menuElement.classList.remove('menu-js-closed');
+    menuElement.classList.add('menu-js-opened');
+  }
+});
